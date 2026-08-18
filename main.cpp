9499484882ebd87e5fb71983316d6dcb3d5ccc48
73. brands <166329422+Eitawz@users.noreply.github.com>
2026-08-18 10:54:49 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+	
+	int idade = 26;
+	float altura = 1.75;
+	char inicial = 'A' ;
+	unsigned int id_usuario = 45500;
+	
+	printf("Idade: %d anos\n", idade);
+	printf("Altura: %.2f metros\n", altura);
+	printf("Inicial do nome: %c\n", inicial);
+	printf("ID do Usuário: %u\n", id_usuario);
+	
+	return 0;
+	
+	}
