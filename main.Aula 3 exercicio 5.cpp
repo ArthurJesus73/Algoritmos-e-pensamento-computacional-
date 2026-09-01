474fc40905e3b0081130df4908244f4e3f7e131c
Arthur Jesus <166329422+ArthurJesus73@users.noreply.github.com>
2026-09-01 09:37:53 -0300

    Add files via upload

---
+++ main.Aula 3 exercicio 5.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+	
+	float peso;
+	float altura;
+	float imc;
+	
+	printf("Digite o seu peso: ");
+	scanf(" %f", &peso);
+	
+	printf("Digite a sua altura: ");
+	scanf(" %f", &altura);
+	
+	imc=peso / pow(altura,2);
+	
+	printf("O indice de massa corporal e: %2f \n", imc);
+	
+	return 0;
+
+	
+}
+
+
